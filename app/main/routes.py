3e05fb7f2515fe1b2aee7a25f61c2ced2183c8dd
Ilya <i.oberemok777@gmail.com>
2022-04-24 11:54:42 +0300

    Add geocoding api to the application

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,17 @@
 from flask import render_template, redirect, url_for
 from app.main import main
 from app.main.forms import SearchForm
+import requests
+import dotenv
+import os
+
+# load .env file from the local system
+dotenv.load_dotenv("C://EnvironmentalVariables//.env")
+
+# constants
+WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
+GEOCODING_API_URL = "http://api.openweathermap.org/geo/1.0/direct"
+API_KEY = os.environ.get("WEATHER_API_KEY")
 
 
 # routes are created with main blueprint, otherwise it won't work
@@ -12,6 +23,18 @@ def home():
     form = SearchForm()
 
     if form.validate_on_submit():
-        return redirect(url_for('main.home'))
+        # params for geocoding api
+        geocoding_params = {
+            'q': form.city.data,
+            'appid': API_KEY,
+            'limit': 1,
+        }
+
+        # make a GET request to the Geocoding API to get the coordinates of the city that user has searched for
+        response = requests.get(url=GEOCODING_API_URL, params=geocoding_params)
+        response.raise_for_status()
+
+        lat, lon = response.json()['lat'], response.json()['lon']
+        print(lat, lon)
 
     return render_template("index.html", form=form), 200
